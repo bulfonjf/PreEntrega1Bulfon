e85977019e7df6959aa2b1b0ced4be5bf6f404b6
bulfonjf <bulfonjf@gmail.com>
2024-06-13 19:02:39 -0300

    Add spinner

--- src/components/ItemListContainer.tsx
+++ src/components/ItemListContainer.tsx
@@ -4,6 +4,7 @@ import { getItems } from '../firebase/firebase';
 import { Item } from '../model/item';
 import ItemList from './ItemList';
 import Alert from './Alert';
+import ReactLoading from 'react-loading';
 
 interface Props {
   greeting: string;
@@ -13,17 +14,23 @@ interface Props {
 export default function ItemListContainer({greeting}: Props) {
 
   const [items, setItems] = useState([] as Item[]);
+  const [isLoading, setIsLoading] = useState(true);
   const {category} = useParams();
 
   useEffect(() => {
     const fetchItems = async () => {
       try {
-        const items = await getItems(category || 'all');
+        setIsLoading(true);
 
-        setItems(items);
-        
+        await getItems(category || 'all').then(
+          (items) => {
+            setIsLoading(false);
+            setItems(items);
+            return items;
+          }
+        );
       } catch (error) {
-        console.error('Error getting items', error)
+        console.error('Error getting items', error);
       }
     };
 
@@ -34,11 +41,16 @@ export default function ItemListContainer({greeting}: Props) {
     <>
       <h1 className="text-3xl">{greeting}</h1>
       <div className="overflow-x-auto">
+        {isLoading && 
+          <div className="container flex justify-center items-center">
+            <ReactLoading type={'spin'} color={'black'} /> 
+          </div>
+        }
         <table className="table table-zebra text-xl">
           <tbody>
             
               {
-                items.length === 0 ? (
+                items.length === 0 && !isLoading? (
                   <tr><td><Alert message='Items not found'/></td></tr>
                 ) : (
                 items.map((item) => (
